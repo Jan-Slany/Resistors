8d3c3220824a573e3837a3eaab55ad143cf3b03e
maruks2003 <marek.slany69@gmail.com>
2022-01-15 19:59:09 +0100

    Priprava funkci na vetsi odpor

--- resistors.py
+++ resistors.py
@@ -1,23 +1,25 @@
-def serial(number1, number2):
-	return number1+number2
+def serial(nums: [float]) -> float:
+	return sum(nums)
 
-def parallel(number1, number2):
-	return round(1 / (1 / number1 + 1 / number2))
+def parallel(nums: [float]) -> float:
+	return round(1/sum([1/i for i in nums]), 2)
 
 prompt = "Zadej cislo\n> "
 
 while True:
 
 	x = input("Paralelne/seriove [p/s], exit pro ukonceni [e]\n> ").lower()
+
+	func = parallel
+	if x.startswith("s"):
+		func = serial
+	elif x.startswith("e"):
+		break
+
 	try:
 		n1 = int(input("> "))
 		n2 = int(input("> "))
 	except Exception as e:
 		print(f"Chyba: nebud dement\n\t{e}")
 
-	if x.startswith("s"):
-		print(serial(n1, n2))
-	elif x.startswith("p`"):
-		print(parallel(n1, n2))
-	elif x.startswith("e"):
-		break
+	print(func([n1, n2]))
