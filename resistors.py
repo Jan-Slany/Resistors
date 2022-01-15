3282f16e5a92b76d24c24015209109b47696e105
maruks2003 <marek.slany69@gmail.com>
2022-01-15 18:55:04 +0100

    Zlepseni inputu

--- resistors.py
+++ resistors.py
@@ -7,23 +7,20 @@ def parallel(number1, number2):
 	z = round(z, 2)
 	print(z)
 
+prompt = "Zadej cislo\n> "
+
 while True:
 
-	x = input("Paralelne/seriove [p/s]\n> ").lower()
-	
+	x = input("Paralelne/seriove [p/s], exit pro ukonceni [e]\n> ").lower()
 	try:
-		if x.startswith("s"):
-			s1 = int(input())
-			s2 = int(input())
-			serial(s1, s2)
-
-		elif x.startswith("p"):
-			p1 = int(input())
-			p2 = int(input())
-			parallel(p1, p2)
-
+		n1 = int(input())
+		n2 = int(input())
 	except Exception as e:
 		print(f"Chyba: nebud dement\n\t{e}")
-	
-	if x.startswith("exit"):
+
+	if x.startswith("s"):
+		serial(n1, n2)
+	elif x.startswith("p`"):
+			parallel(n1, n2)
+	elif x.startswith("e"):
 		break
