aff6d938a5fafb196e3571ba0b7631557e20864a
slany3.jan <97552526+Jan-Slany@users.noreply.github.com>
2022-01-18 22:41:42 +0100

    Update resistors.py

--- resistors.py
+++ resistors.py
@@ -1,36 +1,31 @@
-#funkce na vypočítání sériovích rezistorů
+'''Funkce pro výpočet sériového obvodu'''
 def serial(number1, number2):
-	z = number1+number2
-	print(z)
-	print("")
+    z = number1+number2
+    print(z)
+    print("")
 
-#funkce na vypočítání paralelních rezistorů
+'''Funkce pro výpočet paralelního obvodu'''
 def parallel(number1, number2):
-	z = 1 / (1 / number1 + 1 / number2)
-	z = round(z, 2)
-	print(z)
-	print("")
+    z = 1 / (1 / number1 + 1 / number2)
+    z = round(z, 2)
+    print(z)
+    print("")
 
 while True:
 
-	x = input("Choose: Serial/Parallel ")
+    x = input("Jaký typ obvodu chceš vypočítat sériový/paralelní [s/p] pro ukončení napiš [e]\n> ").lower()
 
-#třídění inputu, na typ rezistorů které chceme vypočítat
+    if x.startswith("s"):
+        s1 = int(input(">"))
+        s2 = int(input(">"))
+        print("――――――")
+        serial(s1, s2)
+  
+    elif x.startswith("p"):
+        p1 = int(input(">"))
+        p2 = int(input(">"))
+        print("――――――")
+        parallel(p1, p2)
 
-	#pokud napíšeme s, aktivuje se funkce na výpočet sériovích rezistorů
-	if x.startswith("s"):
-		s1 = int(input())
-		s2 = int(input())
-		print("――――――")
-		serial(s1, s2)
-
-	#pokud napíšeme p, aktivuje se funkce na výpočet paralelních rezistorů	
-	elif x.startswith("p"):
-		p1 = int(input())
-		p2 = int(input())
-		print("――――――")
-		parallel(p1, p2)
-
-	#ukončení loopu
-	elif x.startswith("e"):
-		break
+    elif x.startswith("e"):
+        break
