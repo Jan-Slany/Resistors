ee8290f99d9b3923f66f14ea2ad1fd04e0598e49
slany3.jan <97552526+Jan-Slany@users.noreply.github.com>
2022-01-30 15:19:27 +0100

    Errors update

--- resistors.py
+++ resistors.py
@@ -16,16 +16,26 @@ while True:
     x = input("Jaký typ obvodu chceš vypočítat sériový/paralelní [s/p] pro ukončení napiš [e]\n> ").lower()
 
     if x.startswith("s"):
-        s1 = int(input(">"))
-        s2 = int(input(">"))
-        print("――――――")
-        serial(s1, s2)
-  
+        try:
+            s1 = int(input(">"))
+            s2 = int(input(">"))
+            print("――――――")
+            serial(s1, s2)
+        except Exception as e:
+            print("---Error---")
+            print(e)
+            print("")
+
     elif x.startswith("p"):
-        p1 = int(input(">"))
-        p2 = int(input(">"))
-        print("――――――")
-        parallel(p1, p2)
+        try:
+            p1 = int(input(">"))
+            p2 = int(input(">"))
+            print("――――――")
+            parallel(p1, p2)
+        except Exception as e:
+            print("---Error---")
+            print(e)
+            print("")
 
     elif x.startswith("e"):
         break
